703f22d7c18c4e45584021016045f3db8e7941ac
erencan-c <erencan.ceyhan@hotmail.com>
2023-07-09 15:54:56 +0300

    Remove the unnecessary instruction creators

--- src/value/instruction.rs
+++ src/value/instruction.rs
@@ -1,63 +1,7 @@
 use crate::value::*;
 use crate::vm::*;
 
-impl KutInstruction {
-    pub fn no_operation() -> KutInstruction {
-        KutInstruction::NoOperation
-    }
-    pub fn mov_register(destination: u8, source: u8) -> KutInstruction {
-        KutInstruction::MovRegister { destination, source }
-    }
-    pub fn call_method_r(ret_position: u8, arg_count: u8, subject: u8) -> KutInstruction {
-        KutInstruction::CallMethodR { ret_position, arg_count, subject }
-    }
-    pub fn call_method_s(arg_count: u8, subject: u8) -> KutInstruction {
-        KutInstruction::CallMethodS { arg_count, subject }
-    }
-    pub fn ret_method_r(value: u8) -> KutInstruction {
-        KutInstruction::RetfMethodR { value }
-    }
-    pub fn ret_method_s() -> KutInstruction {
-        KutInstruction::RetfMethodS
-    }
-    pub fn push_value_1(val1: u8) -> KutInstruction {
-        KutInstruction::PushValue1R { val1 }
-    }
-    pub fn push_value_2(val1: u8, val2: u8) -> KutInstruction {
-        KutInstruction::PushValue2R { val1, val2 }
-    }
-    pub fn push_value_3(val1: u8, val2: u8, val3: u8) -> KutInstruction {
-        KutInstruction::PushValue3R { val1, val2, val3 }
-    }
-    pub fn swap_values(reg1: u8, reg2: u8) -> KutInstruction {
-        KutInstruction::SwapValuesR { reg1, reg2 }
-    }
-    pub fn get_literal(reg: u8, literal: u16) -> KutInstruction {
-        KutInstruction::GetLiteralR { reg, literal }
-    }
-    pub fn get_capture(reg: u8, capture: u16) -> KutInstruction {
-        KutInstruction::GetCaptureR { reg, capture }
-    }
-    pub fn set_capture(reg: u8, capture: u16) -> KutInstruction {
-        KutInstruction::SetCaptureR { reg, capture }
-    }
-    pub fn capture_func(reg: u8, template: u16) -> KutInstruction {
-        KutInstruction::CaptureFunc { reg, template }
-    }
-    pub fn push_literal(literal: u16) -> KutInstruction {
-        KutInstruction::PushLiteral { literal }
-    }
-    pub fn push_capture(capture: u16) -> KutInstruction {
-        KutInstruction::PushCapture { capture }
-    }
-    pub fn push_func_stack(template: u16) -> KutInstruction {
-        KutInstruction::PushFuncStk { template }
-    }
-    pub fn pop_capture(capture: u16) -> KutInstruction {
-        KutInstruction::PopCaptureS { capture }
-    }
-}
-
+/// Instruction executor
 impl KutInstruction {
     pub fn run<'closure, 'template>(&self, context: &mut KutFunction<'closure, 'template>, vm: &'closure KutVm<'template>) -> KutReturnType<'template> {
         match self {
@@ -84,6 +28,7 @@ impl KutInstruction {
 
 }
 
+/// Auxillary functions
 impl<'closure, 'template> KutInstruction {
     fn get_register_value<'reg>(context: &'reg mut KutFunction<'closure, 'template>, reg: u8) -> Result<KutValue<'template>, KutError> {
         if let Some(source) = context.registers.get(reg as usize) {
@@ -119,6 +64,7 @@ impl<'closure, 'template> KutInstruction {
     }
 }
 
+/// Instruction handlers
 impl<'closure, 'template> KutInstruction {
     fn handle_no_operation() -> KutReturnType<'template> {
         Ok(None)
